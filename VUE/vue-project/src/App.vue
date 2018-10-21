44e6dcc9b4233f907ccdc482105ceffb8d0c6279
anabasis <anabasis01@gmail.com>
2018-10-21 17:18:08 +0900

    vue

--- VUE/vue-project/src/App.vue
+++ VUE/vue-project/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
+    <!--
+    <button class="btn btn-primary">Button</button>
     <img src="./assets/logo.png">
+    -->
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'app'
 }
 </script>
 
--- VUE/vue-project/src/router/index.js
+++ VUE/vue-project/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Example from '@/components/Example'
+import TodoPage from '@/components/TodoPage'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/example',
+      name: 'Example',
+      component: Example
+    },
+    { /* 추가 */
+      path: '/todos',
+      name: 'TodoPage',
+      component: TodoPage
     }
   ]
 })
